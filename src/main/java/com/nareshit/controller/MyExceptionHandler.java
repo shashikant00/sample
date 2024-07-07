bec3fc4637735b0c8c095504fa5ba9c626e0fc44
shashikant00 <shashikantgit10@gmail.com>
2024-07-07 12:27:59 +0530

    second commit

--- src/main/java/com/nareshit/controller/MyExceptionHandler.java
+++ src/main/java/com/nareshit/controller/MyExceptionHandler.java
@@ -2,6 +2,8 @@ package com.nareshit.controller;
 
 import com.nareshit.exception.EmployeeNotFoundException;
 import com.nareshit.exception.model.ErrorType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,10 +12,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class MyExceptionHandler {
 
+    private static Logger logger = LoggerFactory.getLogger(MyExceptionHandler.class);
 
     @ExceptionHandler(EmployeeNotFoundException.class)
     public ResponseEntity<ErrorType> empNotFoundExHandler(EmployeeNotFoundException exception){
 
+        logger.info("MyExceptionHandler - ex");
         return new ResponseEntity<>(new ErrorType(exception.getMessage(),
                 "EMPLOYEE ID NOT EXISt","EMPLOYEE",
                 "EMPLOYEE ID NOT FOUND"),
